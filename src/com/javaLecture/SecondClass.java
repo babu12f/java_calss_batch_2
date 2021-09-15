e4cc50a336a6940482fc4f85a4f3f9c73a5311c9
Shofiullah Babor <babu_12f@yahoo.com>
2021-09-15 23:12:06 +0600

    batch 2 class 2 complete -> variable, leteral, type cusingn,

---
+++ src/com/javaLecture/SecondClass.java
@@ -0,0 +1,46 @@
+package com.javaLecture;
+
+public class SecondClass {
+
+    public static void main(String[] sp){
+        //Variable
+        /*
+        * a = 12
+        * b = 10
+        *
+        * */
+        //System.out.println(a + ", " + b + ", " + c + ", " + n);
+
+        int a = 5, b = 2, c = 90;
+        double n = 34.6;
+
+        //int ans = 1 + 2;
+        //System.out.println(ans);
+
+        /*int div = 5 / 2;
+        System.out.println(div);//2.5*/
+
+       /* float div =  (float) a / b;
+        System.out.println(div);*/
+
+       /*int rem = 13 % 5;
+       System.out.println(rem);*/
+
+        int i = 2;
+        i++; // i = i+1;
+
+        int j = --i;
+
+        System.out.println(j);
+        System.out.println(i);
+
+        //
+        int x = 12;
+        int y = 23;
+
+
+
+
+    }
+
+}
