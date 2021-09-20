8dfc84253cb572c3bd1c2e3f75125a58ae38409e
Shofiullah Babor <babu_12f@yahoo.com>
2021-09-20 23:06:14 +0600

    class 4 complete

---
+++ src/com/javaLecture/Class4.java
@@ -0,0 +1,71 @@
+package com.javaLecture;
+
+import java.util.Scanner;
+
+public class Class4 {
+    public static void main(String[] args) {
+
+        /*
+        *
+        * 9 * 1 = 9
+        * 9 * 2 = 18
+        * 9 * 3 = 27
+        * 9 * 4 = 36
+        * ....
+        *
+        * */
+
+        /*int i = 1;
+        while( i <= 100 ) {
+            System.out.println(i);
+            i = i + 2;
+        }*/
+
+        /*int i = 1;
+        while (i <= 10) {
+            int rs = i * 9;
+
+            System.out.println("9 * " + i + " = "+ rs);
+            i++;
+        }*/
+
+        // for loop
+
+
+        /*for (int i = 1;  i <= 100;  i = i + 2) {
+            System.out.println(i);
+        }*/
+
+
+        /*
+
+        #####
+        #####
+        #####
+        #####
+        #####
+
+        * */
+
+        /*for (int i = 1; i <=5; i++) {
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("#");
+            }
+            System.out.print("\n");
+
+        }*/
+
+        int x = 80;
+
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i <= 10);
+
+
+    }
+
+}
