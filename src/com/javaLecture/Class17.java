1050e3f19a3e9078d3e7a607cdb27eb011a9ce31
Shofiullah Babor <babu_12f@yahoo.com>
2021-11-10 23:06:57 +0600

    class 17 complete -->>> database first class

---
+++ src/com/javaLecture/Class17.java
@@ -0,0 +1,47 @@
+package com.javaLecture;
+
+public class Class17 {
+
+    public static void main(String[] args) {
+        /*
+        * database
+        *
+        *
+        create table person_info(
+            id int,
+            name varchar(40),
+            age int,
+            address varchar(300)
+        );
+
+        DROP DATABASE java_batch_2;
+
+        DROP TABLE person_info;
+
+        insert into person_info (name, id, age, address)
+                                values('ruhit', 1, 29, 'dhaka');
+
+        create table person_info(
+            id int primary key auto_increment,
+            name varchar(40),
+            age int,
+            address varchar(300)
+        );
+
+        select * from person_info where age  >= 30 and age <=50;
+
+        update person_info
+        set
+        name = 'ruhit',
+        age = 19,
+        address='rajsahi'
+
+        where id = 1;
+
+        delete from person_info where id = 8;
+
+        *
+        * */
+    }
+
+}
